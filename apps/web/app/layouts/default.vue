fe187995a54492318704540ebe118d9ccbb66e40
KUN1007 <yuyuyukunkunkun@gmail.com>
2026-05-07 07:46:09 +0800

    mod(web): remove breadcrumb

--- apps/web/app/layouts/default.vue
+++ apps/web/app/layouts/default.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="bg-radial relative flex min-h-screen flex-col items-center">
     <KunTopBar />
-    <KunNavigationBreadcrumb />
 
     <div
-      class="flex min-h-[calc(100dvh-24rem)] w-full max-w-7xl grow px-3"
+      class="mt-4 flex min-h-[calc(100dvh-24rem)] w-full max-w-7xl grow px-3"
     >
       <slot />
     </div>
